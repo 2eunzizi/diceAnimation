b8b70bcc33b082c27539b4a3f5b1acd2579a7dc9
Michelle <2eunzizi@gmail.com>
2018-06-21 14:03:21 -0400

    Updated

--- diceAnimation/ViewController.swift
+++ diceAnimation/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func button(_ sender: Any) {
+        let hopAnimation:[UIImage] = [
+            UIImage(named: "dice1.jpg")!,
+            UIImage(named: "dice2.jpg")!,
+            UIImage(named: "dice3.jpg")!,
+            UIImage(named: "dice4.jpg")!,
+            UIImage(named: "dice5.jpg")!,
+            UIImage(named: "dice6.jpg")!,
+            ]
+        myImage.animationImages = hopAnimation
+        myImage.animationDuration = 1.0
+        myImage.animationRepeatCount = 4
+        myImage.startAnimating()
+        myImage.image = UIImage(named: "dice2.jpg")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
